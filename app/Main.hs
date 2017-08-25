bd14f4a675573a233175b808432974d11de37c25
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2017-08-25 23:53:09 +0200

    Got MIME types working!

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,54 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Main where
 
-import Network.Wai.Handler.Warp
-       (defaultSettings, runSettings, setPort)
+import Control.Arrow (left)
+
+import Control.Monad.Catch (throwM)
+import qualified Data.Text.Lazy.Encoding as LText (decodeUtf8')
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Servant ((:>), PlainText, Post, ReqBody, Server, serve)
+import Servant
+       ((:<|>)((:<|>)), (:>), Accept(contentType), Get, Handler, JSON,
+        MimeRender(mimeRender), MimeUnrender(mimeUnrender), PlainText,
+        Post, ReqBody, Server, serve)
 import System.Environment (getEnv)
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Text.Markdown (def, markdown)
 
 import Protolude
 
-type Api = "md" :> ReqBody '[ PlainText] Text :> Post '[ PlainText] Text
+type Api
+   = "md" :> ReqBody '[ Markdown, PlainText] LText :> Post '[ Html, PlainText] LText :<|> "uploadmd" :> Get '[ PlainText] LText
+
+data Html
+
+data Markdown
+
+instance Accept Html where
+  contentType _ = "text" // "html" /: ("charset", "utf-8")
+
+instance Accept Markdown where
+  contentType _ = "text" // "markdown" /: ("charset", "utf-8")
+
+instance MimeRender Html LText where
+  mimeRender _ = strConv Lenient
+
+instance MimeUnrender Markdown LText where
+  mimeUnrender _ = left show . LText.decodeUtf8'
 
 type Port = Int
+
+data ConfigException =
+  ConfigException Text
+  deriving (Show)
+
+instance Exception ConfigException
 
 main :: IO ()
 main = do
@@ -23,7 +57,7 @@ main = do
     case readMaybe @Int portString of
       Just someInt -> pure someInt
       Nothing ->
-        error $
+        throwM . ConfigException $
         "Environment variable PORT must be an int, but was: " <>
         strConv Lenient portString
   startServer port
@@ -31,8 +65,11 @@ main = do
 startServer :: Port -> IO ()
 startServer port = do
   putText $ "Starting server on port " <> show port
-  let settings = setPort port defaultSettings
-  runSettings settings $ logStdoutDev (serve (Proxy @Api) server)
+  run port $ logStdoutDev (serve (Proxy @Api) server)
 
 server :: Server Api
-server = pure
+server = mdConverter :<|> mdUploader
+
+mdUploader = pure . renderHtml $ markdown def "# Upload Markdown"
+
+mdConverter = pure . renderHtml . markdown def
